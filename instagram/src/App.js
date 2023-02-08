5b7e05d5447fa8d2b75f37dbc87f25979dece2ba
vincent <vincentxavier2602@gmail.com>
2023-02-08 17:14:21 +0530

    Creating Global Theme

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -3,13 +3,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { fakeApi } from "./theme/themeSlice";
 import Theme from "./theme/theme";
 import "./App.css";
+import GoogleMapReact from "google-map-react";
 
 function App() {
   const dispatch = useDispatch();
   const { isLoading, data } = useSelector((state) => state);
+
+  var defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 2,
+  };
+
   return (
     <>
-      <ThemeProvider theme={(Theme) => Theme}>
+      <ThemeProvider theme={Theme}>
         <Button
           variant="contained"
           color="primary"
@@ -17,6 +27,19 @@ function App() {
         >
           Api Call!
         </Button>
+
+        <div
+          className="d-flex justify-content-center "
+          style={{ height: "100vh", width: "100%" }}
+        >
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: "" }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+            center={defaultProps.center}
+          >
+          </GoogleMapReact>
+        </div>
 
         {isLoading && <h3>Loading ...</h3>}
         <List>
